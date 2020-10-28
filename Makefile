0a0fc120510d1d0ecc577c25754d9888ab3a244a
Timur <t.hslitov@yandex.ru>
2020-10-28 19:27:59 +0400

    github action

--- Makefile
+++ Makefile
@@ -6,3 +6,7 @@ publish:
 	npm publish --dry-run
 cli:
 	node src/cli.js
+lint:
+	npx eslint .
+lint-fix:
+	npx eslint --fix .
--- index.js
+++
@@ -1,5 +0,0 @@
-import promptly from 'promptly';
-
-const async function () => {
-    const name = await promptly.prompt('May I have your name?: ');
-  };
