d9b8c66a31c9a0ec984042ca1aaedbdc0f1b3589
Timur <t.hslitov@yandex.ru>
2020-11-07 00:34:58 +0400

    fix

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import async from '../src/games/calc.js';
 
-
 console.log('Welcome to the Brain Games!');
 
 const calc = async();
--- src/games/calc.js
+++ src/games/calc.js
@@ -1,6 +1,5 @@
 import promptly from 'promptly';
 
-
 export default async () => {
   const name = await promptly.prompt('May I have your name?: ');
   console.log(`Hello, ${name}`);
@@ -13,18 +12,19 @@ export default async () => {
     const quest1 = await promptly.prompt(`Question: ${selfRandom1} ${randomElement} ${selfRandom}: `);
     console.log(`Your answer: ${quest1}`);
     let result;
-    switch (randomElement) { 
-      case '+' :
-      result = Number(selfRandom1) + Number(selfRandom);
-      console.log(result);
-      break;
-      case '-' :
-      result = Number(selfRandom1) - Number(selfRandom);
-      break;
-      case '*' :
-      result = Number(selfRandom1) * Number(selfRandom);
-      console.log(result);
-      default :
+    switch (randomElement) {
+      case '+':
+        result = Number(selfRandom1) + Number(selfRandom);
+        console.log(result);
+        break;
+      case '-':
+        result = Number(selfRandom1) - Number(selfRandom);
+        break;
+      case '*':
+        result = Number(selfRandom1) * Number(selfRandom);
+        console.log(result);
+        break;
+      default:
     }
     if (Number(quest1) === result) {
       console.log('Correct!');
